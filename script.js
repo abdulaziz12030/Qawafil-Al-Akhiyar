b08fd73f8fa2bba8cc1598b75a140ebef86ac9e8
abdulaziz12030 <abdulaziz.algharawi@gmail.com>
2025-10-20 20:27:51 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function sendToWhatsApp() {
-    const phoneNumber = '966570905999';
-    const message = encodeURIComponent('السلام عليكم، أرغب في المساهمة في مشاريع قوافل الأخيار');
-    const url = `https://wa.me/${phoneNumber}?text=${message}`;
-    window.open(url, '_blank');
-}
